4d1052d33a74f6105adc3135ecd2944afe92e1f8
sam <sammyrulez@gmail.com>
2015-04-30 09:38:17 +0200

    testing

--- db-backend/src/test/scala/scalatrabouncer/db/DaoDbTest.scala
+++ db-backend/src/test/scala/scalatrabouncer/db/DaoDbTest.scala
@@ -8,19 +8,27 @@ import org.specs2.mutable.Before
 
 @RunWith(classOf[JUnitRunner])
 class DaoDbTest extends Specification with Before {
-
+  val db = Database.forURL("jdbc:h2:mem:hello;DB_CLOSE_DELAY=-1", driver = "org.h2.Driver")
+  val dao: UserDaoDb = new UserDaoDb(H2Driver, db)
+  dao.createDb()
   def before() = {
 
   }
 
   "A dao db" should {
     " load user from db " in {
-      val db = Database.forURL("jdbc:h2:mem:hello;DB_CLOSE_DELAY=-1", driver = "org.h2.Driver")
-      val dao: UserDaoDb = new UserDaoDb(H2Driver, db)
-      dao.createDb()
+
+
       val adminUsr = dao.loadUser("admin")
+
       adminUsr.username mustEqual "admin"
+
     }
+    " load roles from db " in {
+      val roles = dao.userRoles("admin")
+      roles contains("USER_ADMIN")
+    }
+
 
   }
 
